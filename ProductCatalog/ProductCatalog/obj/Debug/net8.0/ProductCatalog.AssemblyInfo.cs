486855491845a1636d890d6bb35d82572a7e6d43
Antoni <antoni.jakowicz@cyfroweszkoly.pl>
2024-11-11 21:40:03 +0100

    rozpoczecie aplikacji product catalog

--- ProductCatalog/ProductCatalog/obj/Debug/net8.0/ProductCatalog.AssemblyInfo.cs
+++ ProductCatalog/ProductCatalog/obj/Debug/net8.0/ProductCatalog.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ProductCatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a613fddc7028b9ca638bdd6ecc12a163d4991d")]
-[assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("ProductCatalog")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ProductCatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
